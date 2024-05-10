584018260a49a24153667ede612de73b2ba47acc
Premanand Prabhakaran <82953912+premananthsvcece@users.noreply.github.com>
2024-05-10 19:15:09 +0530

    Delete ZPTP_PRPO_CYCLE_REP directory

--- ZPTP_PRPO_CYCLE_REP/dist/Component.js
+++
@@ -1,9 +0,0 @@
-jQuery.sap.declare("ZPTP_PRPO_CYCLE_REP.Component");
-sap.ui.getCore().loadLibrary("sap.ui.generic.app");
-jQuery.sap.require("sap.ui.generic.app.AppComponent");
-
-sap.ui.generic.app.AppComponent.extend("ZPTP_PRPO_CYCLE_REP.Component", {
-	metadata: {
-		"manifest": "json"
-	}
-});
